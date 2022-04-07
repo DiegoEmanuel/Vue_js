8ad5fddf51170f9b315c87b1ae61c821a17fcf32
Diego Emanuel <diegoabox@gmail.com>
2022-04-07 14:42:25 -0300

    Envio de arquivos iniciais com a tela de registro

---
+++ src/components/FormValidation.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="card">
+    <h3 class="card-header text-center" >Area de Registro</h3>
+    <div class="card-body">
+      <form>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label>Firstname</label>
+            <input type="text" class="form-control" v-model.trim="$v.firstname.$model" :class="
+            {'is-invalid':$v.firstname.$error, 'is-invalid':!$v.firstname.$invalid }">
+            <div class="valid-feedback">Your first name is valid!</div>
+            <div class="invalid-feedback">Your first name is invalid!</div>
+              <span v-if="!$v.firstname.required">First name is required</span>
+              <span v-if="!$v.firstname.minLength.min">First name must have at less</span>
+              <span v-if="!$v.firstname.maxLength.max">First name must have at most</span>
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label>Lastname</label>
+            <input type="text" class="form-control" v-model.trim="$v.lastname.$model" :class="
+            {'is-invalid':$v.lastname.$error, 'is-invalid':!$v.lastname.$invalid }">
+            <div class="valid-feedback">Your last name is valid!</div>
+            <div class="invalid-feedback">Your Last name is invalid!</div>
+              <span v-if="!$v.lastname.required">Last name is required</span>
+              <span v-if="!$v.lastname.minLength.min">Last name must have at less</span>
+              <span v-if="!$v.lastname.maxLength.max">Last name must have at most</span>
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="form-group">
+            <label>age</label>
+            <input type="number" class="form-control" v-model.number.lazy="$v.age.$model" :class="
+            {'is-invalid':$v.age.$error, 'is-invalid':!$v.age.$invalid }">
+            <div class="valid-feedback">Your last name is valid!</div>
+            <div class="invalid-feedback">Your last name is invalid!</div>
+              <span v-if="!$v.age.required">age name is required</span>
+              <span v-if="!$v.age.minLength.min">age name must have at less</span>
+              <span v-if="!$v.age.maxLength.max">age name must have at most</span>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+  import{required, minLength, maxLength, between} from 'vuelidate/lib/validators'
+
+  export default{
+    name: 'FormValidation',
+    data (){
+      return{
+        firstname: '',
+        lastname: '',
+        age: 0
+      }
+    },
+    validations:{
+      required,
+      minLength:minLength(3),
+      maxLength:maxLength(10)
+    },
+    lastname:{
+      required,
+      minLength:minLength(4),
+      maxLength:maxLength(10)
+    },
+    age:{
+      between:between(15,40)
+    }
+  }
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import vuetify from './plugins/vuetify'
+Vue.use(Vuelidate)
+Vue.config.production = false
 
-new Vue({
-  render: h => h(App),
+
+new Vue ({
+  vuetify,
+  render: h => h (App)
 }).$mount('#app')
